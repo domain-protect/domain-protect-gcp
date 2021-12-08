58e91f629b5890a625b1a2eba839a102acbda2ed
Paul Schwarzenberger <paul@celidor.net>
2021-12-08 09:41:10 +0000

    OSE-309 python improvements

--- manual-scans/gcp-cname-storage.py
+++ manual-scans/gcp-cname-storage.py
@@ -7,7 +7,6 @@ from utils_print import my_print, print_list
 vulnerable_domains = []
 suspected_domains = []
 cname_values = []
-global vulnerability_list
 vulnerability_list = ["amazonaws.com", "cloudfront.net", "c.storage.googleapis.com"]
 
 
@@ -20,51 +19,52 @@ def vulnerable_storage(domain_name):
     return False
 
 
-class gcp:
-    def __init__(self, project):
-        self.project = project
-        i = 0
+def gcp(project):
+    i = 0
 
-        print(f"Searching for Google Cloud DNS hosted zones in {project} project")
-        dns_client = google.cloud.dns.client.Client(project=self.project)
-        try:
-            managed_zones = dns_client.list_zones()
+    print(f"Searching for Google Cloud DNS hosted zones in {project} project")
+    dns_client = google.cloud.dns.client.Client(project)
+    try:
+        managed_zones = dns_client.list_zones()
 
-            for managed_zone in managed_zones:
-                print(f"Searching CNAMEs with missing storage buckets in {managed_zone.dns_name}")
+        for managed_zone in managed_zones:
+            print(f"Searching CNAMEs with missing storage buckets in {managed_zone.dns_name}")
 
-                dns_record_client = google.cloud.dns.zone.ManagedZone(name=managed_zone.name, client=dns_client)
+            dns_record_client = google.cloud.dns.zone.ManagedZone(name=managed_zone.name, client=dns_client)
 
-                if dns_record_client.list_resource_record_sets():
+            if dns_record_client.list_resource_record_sets():
 
-                    records = dns_record_client.list_resource_record_sets()
-                    resource_record_sets = [r for r in records if "CNAME" in r.record_type]
+                records = dns_record_client.list_resource_record_sets()
+                resource_record_sets = [
+                    r
+                    for r in records
+                    if "CNAME" in r.record_type
+                    and any(vulnerability in r.rrdatas[0] for vulnerability in vulnerability_list)
+                ]
+                for resource_record_set in resource_record_sets:
+                    cname_record = resource_record_set.name
+                    cname_value = resource_record_set.rrdatas[0]
+                    print(f"Testing {resource_record_set.name} for vulnerability")
+                    result = vulnerable_storage(cname_record)
+                    i = i + 1
+                    if result:
+                        vulnerable_domains.append(cname_record)
+                        cname_values.append(cname_value)
+                        my_print(
+                            f"{str(i)}. {cname_record} CNAME {cname_value}",
+                            "ERROR",
+                        )
+                    elif not result:
+                        suspected_domains.append(cname_record)
+                        my_print(
+                            f"{str(i)}. {cname_record} CNAME {cname_value}",
+                            "SECURE",
+                        )
+                    else:
+                        my_print("WARNING: no response from test", "INFOB")
 
-                    for resource_record_set in resource_record_sets:
-                        if any(vulnerability in resource_record_set.rrdatas[0] for vulnerability in vulnerability_list):
-                            cname_record = resource_record_set.name
-                            cname_value = resource_record_set.rrdatas[0]
-                            print(f"Testing {resource_record_set.name} for vulnerability")
-                            result = vulnerable_storage(cname_record)
-                            i = i + 1
-                            if result:
-                                vulnerable_domains.append(cname_record)
-                                cname_values.append(cname_value)
-                                my_print(
-                                    f"{str(i)}. {cname_record} CNAME {cname_value}",
-                                    "ERROR",
-                                )
-                            elif not result:
-                                suspected_domains.append(cname_record)
-                                my_print(
-                                    f"{str(i)}. {cname_record} CNAME {cname_value}",
-                                    "SECURE",
-                                )
-                            else:
-                                my_print("WARNING: no response from test", "INFOB")
-
-        except google.api_core.exceptions.Forbidden:
-            pass
+    except google.api_core.exceptions.Forbidden:
+        pass
 
 
 if __name__ == "__main__":
